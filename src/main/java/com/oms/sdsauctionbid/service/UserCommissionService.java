852271afa817c54cefc194f11a8e0267fa6c0bbe
vaibhav.tube9@omsoftware.net <vaibhav.tube9@omsoftware.net>
2020-03-17 00:55:39 -0400

    WIP

--- src/main/java/com/oms/sdsauctionbid/service/UserCommissionService.java
+++ src/main/java/com/oms/sdsauctionbid/service/UserCommissionService.java
@@ -12,28 +12,25 @@ import org.springframework.stereotype.Service;
 import java.time.Instant;
 import java.util.Map;
 
+import static com.oms.sdsauctionbid.utils.TransactionType.*;
+
 @Service
 public class UserCommissionService {
 
         private AccountTransactionRepository accountTransactionRepository;
+        private UserAccountTransactionService userAccountTransactionService;
 
     public UserCommissionService(AccountTransactionRepository accountTransactionRepository) {
         this.accountTransactionRepository = accountTransactionRepository;
     }
 
-    public void calculateCommissionForTransaction(Map<Integer,User> userCommissionMap, Map<String, Integer> transactionAmountMap) {
+    public void assignCommissionForTransaction(Map<Integer,User> userCommissionMap,
+                                                  Map<String, Integer> transactionAmountMap) {
       transactionAmountMap.forEach((k,v) -> {
           userCommissionMap.forEach((commissionK,commissionV) -> {
-              UserAccountTransaction userAccountTransaction = new UserAccountTransaction();
-              userAccountTransaction.setTransactionStatus(true);
-              userAccountTransaction.setTransactionAmount((double)(commissionK*v)/100);
-              userAccountTransaction.setTransactionDetails("commission");
-              userAccountTransaction.setTransactionId(k);
-              userAccountTransaction.setTransactionType(TransactionType.BROKERAGE);
-              DateTime dateTime = new DateTime(); // Initializes with the current date and time
-              DateTimeFormatter customFormatter = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ssZ");
-              userAccountTransaction.setTransactionDateTime(customFormatter.print(dateTime));
-              userAccountTransaction.setTransactionEpochTime(Instant.now().toEpochMilli());
+              userAccountTransactionService.processAccountTransactionForUser(commissionV, k,
+                      (double)(commissionK*v)/100, true, "Commission",
+                        BROKERAGE);
           });
         });
     }
