7a3b65df7e969a3e6d2e056173d489cdd0661c6a
auction_oms <arinfotech605@gmail.com>
2020-03-26 22:39:36 -0400

    WIP

--- src/main/java/com/oms/sdsauctionbid/repository/UserRepository.java
+++ src/main/java/com/oms/sdsauctionbid/repository/UserRepository.java
@@ -6,10 +6,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
     @Query(value = "select * from sds_user_details u where u.user_id = ?",
             nativeQuery = true)
     User getUserByUserId(String userId);
 
+    @Query(value = "select * from sds_user_details u where u.user_type_id = ?",
+            nativeQuery = true)
+    List<User> getUsersByUserType(String userTypeId);
 }
