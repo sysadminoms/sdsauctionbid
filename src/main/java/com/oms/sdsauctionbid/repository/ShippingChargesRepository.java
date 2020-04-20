13d76de00722ec1c845d4ee74e57dab5fad204cd
vaibhav.tube9@omsoftware.net <vaibhav.tube9@omsoftware.net>
2020-04-19 22:41:28 -0400

    WIP

--- src/main/java/com/oms/sdsauctionbid/repository/ShippingChargesRepository.java
+++ src/main/java/com/oms/sdsauctionbid/repository/ShippingChargesRepository.java
@@ -11,6 +11,6 @@ public interface ShippingChargesRepository extends CrudRepository<ShippingCharge
     @Query(value = "SELECT SHIPPING_CHARGES.price,SHIPPING_CHARGES.price_type\n" +
             "FROM sds_shipping_charges AS SHIPPING_CHARGES\n" +
             "LEFT JOIN sds_state AS STATE\n" +
-            "ON STATE.id = SHIPPING_CHARGES.state_id where STATE.state_name=? and SHIPPING_CHARGES.type = ?",nativeQuery = true)
+            "ON STATE.id = SHIPPING_CHARGES.state_id where STATE.state_name=? and UPPER(SHIPPING_CHARGES.type) = ?",nativeQuery = true)
     List<Object[]> findByStateIdAndType(String stateName, String type);
 }
